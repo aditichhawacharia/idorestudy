be3813a4fda970f950cd8835f1e465b167e4a784
Aditi Chhawacharia <143767518+aditichhawacharia@users.noreply.github.com>
2026-03-05 23:24:27 +0000

    try

--- study-cafe/src/App.jsx
+++ study-cafe/src/App.jsx
@@ -216,12 +216,14 @@ function YTBackground({ videoId }) {
     <div style={{ position: 'absolute', inset: 0, overflow: 'hidden', background: '#000' }}>
       <iframe
         width="100%" height="100%"
-        src={`https://www.youtube.com/embed/${cleanVideoId}?autoplay=1&mute=1&controls=0&loop=1&playlist=${cleanVideoId}&rel=0&modestbranding=1&showinfo=0&playsinline=1`}
+        src={`https://www.youtube.com/embed/${cleanVideoId}?autoplay=1&mute=1&controls=0&loop=1&playlist=${cleanVideoId}&rel=0&modestbranding=1&showinfo=0&playsinline=1&iv_load_policy=3`}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
+        webkit-playsinline="true"
+        playsInline
         style={{
           position: 'absolute', top: '45%', left: '50%',
           transform: 'translate(-50%, -50%)',
@@ -229,7 +231,6 @@ function YTBackground({ videoId }) {
           border: 'none', pointerEvents: 'none',
         }}
       />
-      <div style={{ position: 'absolute', inset: 0, zIndex: 2 }} />
     </div>
   );
 }
